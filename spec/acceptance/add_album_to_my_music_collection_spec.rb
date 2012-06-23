8014486f532992ea1b6e6d2125b8296b9359c4fb
Vicente Reig Rincón de Arellano <vicente.reig@gmail.com>
2012-06-23 15:13:33 +0200

    rails g steak:spec add_album_to_my_music_collection

---
+++ spec/acceptance/add_album_to_my_music_collection_spec.rb
@@ -0,0 +1,13 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Add album to my music collection', %q{
+  In order to ...
+  As a ...
+  I want ...
+} do
+
+  scenario 'first scenario' do
+    true.should == true
+  end
+
+end
