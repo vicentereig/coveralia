f9b57cc484f16aa95683f28eae5e02ad43cc9c52
Vicente Reig Rincón de Arellano <vicente.reig@gmail.com>
2012-06-23 14:49:38 +0200

    rails new coveralia --skip-test-unit --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coveralia::Application.config.secret_token = 'db01f18cb0ed428c77ce6afa04ad47a5b9d305549b7004ddfb1fd1faf4ab95d7c3a1e56162977f7e4da8479c5736db681bbe09d20ab98395d037ecca25f0dce4'
